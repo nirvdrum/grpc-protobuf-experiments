c35c0d3c342064b98e15738828ecad4275bbabe9
Randy Stauner <randy.stauner@shopify.com>
2025-01-30 09:49:15 -0700

    Move memory usage code into helpers lib for reuse in benchmarks

--- leak-bigtable.rb
+++ leak-bigtable.rb
@@ -2,27 +2,19 @@
 # frozen_string_literal: true
 
 require "google/cloud/bigtable"
-require "objspace"
 
 require_relative "lib/memsize_helpers"
 
 clone_graph = !!ENV['CLONE_GRAPH']
 
 filter = Google::Cloud::Bigtable::V2::RowFilter.new
-memsize_rss_start = MemsizeHelpers.memsize_rss_in_kb
-memsize_rss_current = memsize_rss_start
 
 20.times do
   10_000.times do
     Google::Cloud::Bigtable::V2::RowFilter::Chain.new(filters: [clone_graph ? Google::Protobuf.deep_copy(filter) : filter])
   end
 
-  GC.start
-  memsize_rss_current = MemsizeHelpers.memsize_rss_in_kb
-
-  if ENV["VERBOSE"]
-    puts "Memory usage: #{memsize_rss_current} KB - ruby space #{(ObjectSpace.memsize_of_all / 1000).round(0)} KB"
-  end
+  MemsizeHelpers.check_usage
 end
 
-puts "Total memory growth: #{memsize_rss_current - memsize_rss_start} KB"
+MemsizeHelpers.report_usage
--- lib/memsize_helpers.rb
+++ lib/memsize_helpers.rb
@@ -1,6 +1,41 @@
+require "objspace"
+
 module MemsizeHelpers
   def self.memsize_rss_in_kb
     _, size = %x(ps -p "#{$$}" -o pid=,rss=).strip.split.map(&:to_i)
     size
+  end
+
+  def self.reset!
+    @memsize_rss_start = memsize_rss_in_kb
+  end
+  reset!
+
+  def self.check_usage
+    GC.start
+    if verbose?
+      # Put newline first to keep these aligned in the middle of the benchmkark-ips output.
+      printf "\n[Memory usage: %11s KB - ruby space %7s KB]",
+        format_number(MemsizeHelpers.memsize_rss_in_kb),
+        format_number((ObjectSpace.memsize_of_all / 1000).round(0))
+    end
+  end
+
+  def self.format_number(n)
+    n.to_s.reverse.scan(/\d{1,3}/).join(",").reverse
+  end
+
+  def self.report_usage
+    GC.start
+    printf "Total memory growth: %12s KB\n",
+      format_number(MemsizeHelpers.memsize_rss_in_kb - @memsize_rss_start)
+
+    if verbose? && defined?(Benchmark)
+      puts "\n\nNOTE: Benchmark speed results will be wildly skewed by the ObjectSpace.memsize_of_all call performed in VERBOSE mode and should be discarded.\n\n"
+    end
+  end
+
+  def self.verbose?
+    ENV["VERBOSE"]
   end
 end
