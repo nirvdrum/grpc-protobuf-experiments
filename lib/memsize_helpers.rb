2fe2444a1046bfb8001d678dac04e5838149d582
Randy Stauner <randy@r4s6.net>
2025-01-30 20:16:15 -0700

    Further simplify getting rss

--- lib/memsize_helpers.rb
+++ lib/memsize_helpers.rb
@@ -1,6 +1,5 @@
 module MemsizeHelpers
   def self.memsize_rss_in_kb
-    _, size = %x(ps -p "#{$$}" -o pid=,rss=).strip.split.map(&:to_i)
-    size
+    %x(ps -p "#{$$}" -o rss=).strip.to_i
   end
 end
